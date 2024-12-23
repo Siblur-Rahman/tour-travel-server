b89769633156c9e94797edb29809ae5acbe6d187
MS Rahman <msrahman048gmail.com>
2024-12-23 23:21:49 +0600

    after using catchAsync function in usercontroller.ts

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,101 +1,79 @@
 // req and res manage
 
-import { NextFunction, Request, Response } from 'express'
 import { userService } from './user.service'
 import sendResponse from '../utils/sendResponse'
 import { StatusCodes } from 'http-status-codes'
+import catchAsync from '../utils/catchAsync'
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const payload = req.body
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const payload = req.body
 
-    const result = await userService.createUser(payload)
+//     const result = await userService.createUser(payload)
 
-    // res.json({
-    //   status: true,
-    //   message: 'User created successfully',
-    //   data: result,
-    // })
+//     sendResponse(res, {
+//       statusCode: StatusCodes.CREATED,
+//       message: 'User created successfully',
+//       data: result,
+//     })
+//   } catch (error) {
+//     next(error)
+//   }
+// }
+const createUser = catchAsync(async (req, res) => {
+  const payload = req.body
 
-    sendResponse(res, {
-      statusCode: StatusCodes.CREATED,
-      message: 'User created successfully',
-      data: result,
-    })
-  } catch (error) {
-    next(error)
-    // res.json({
-    //   status: false,
-    //   message: 'Something went wrong',
-    //   error,
-    // })
-  }
-}
+  const result = await userService.createUser(payload)
 
-const getUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const result = await userService.getUser()
+  sendResponse(res, {
+    statusCode: StatusCodes.CREATED,
+    message: 'User created successfully',
+    data: result,
+  })
+})
 
-    sendResponse(res, {
-      statusCode: StatusCodes.OK,
-      message: 'Users getting successfully',
-      data: result,
-    })
-  } catch (error) {
-    next(error)
-  }
-}
+const getUser = catchAsync(async (req, res) => {
+  const result = await userService.getUser()
 
-const getSingleUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    console.log(req.params)
-    const userId = req.params.userId
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    message: 'Users getting successfully',
+    data: result,
+  })
+})
 
-    const result = await userService.getSingleUser(userId)
-    sendResponse(res, {
-      statusCode: StatusCodes.OK,
-      message: 'Users getting successfully',
-      data: result,
-    })
-  } catch (error) {
-    next(error)
-  }
-}
+const getSingleUser = catchAsync(async (req, res) => {
+  const userId = req.params.userId
+  const result = await userService.getSingleUser(userId)
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    message: 'Users getting successfully',
+    data: result,
+  })
+})
 
-const updateUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const userId = req.params.userId
-    const body = req.body
-    const result = await userService.updateUser(userId, body)
+const updateUser = catchAsync(async (req, res) => {
+  const userId = req.params.userId
+  const body = req.body
+  const result = await userService.updateUser(userId, body)
 
-    sendResponse(res, {
-      statusCode: StatusCodes.OK,
-      message: 'User updated successfully',
-      data: result,
-    })
-  } catch (error) {
-    next(error)
-  }
-}
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    message: 'User updated successfully',
+    data: result,
+  })
+})
 
-const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const userId = req.params.userId
-    await userService.deleteUser(userId)
+const deleteUser = catchAsync(async (req, res) => {
+  const userId = req.params.userId
+  await userService.deleteUser(userId)
 
-    sendResponse(res, {
-      statusCode: StatusCodes.OK,
-      message: 'User deleted successfully',
-      data: {},
-    })
-  } catch (error) {
-    next(error)
-  }
-}
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    message: 'User deleted successfully',
+    data: {},
+  })
+})
 
 export const userController = {
   createUser,
