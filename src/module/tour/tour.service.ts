7641f5b03c034c7b38bd5ff0c52249fc71876f36
MS Rahman <msrahman048gmail.com>
2024-12-28 14:55:38 +0600

    select fields topic completed

--- src/module/tour/tour.service.ts
+++ src/module/tour/tour.service.ts
@@ -12,8 +12,84 @@ const createTour = async (payload: ITour) => {
   return result
 }
 
-const getTours = async () => {
-  const result = Tour.find()
+const getTours = async (query: Record<string, unknown>) /** 17;52 */ => {
+  const queryObj = { ...query }
+
+  const excludingImportant = [
+    'searchTerm',
+    'page',
+    'limit',
+    'sortBy',
+    'sortOrder',
+    'fields',
+  ]
+
+  excludingImportant.forEach((key) => delete queryObj[key])
+
+  const searchTerm = query?.searchTerm || ''
+
+  /* 
+  const result = await Tour.find({
+    //   $or: [
+      //     { name: { $regex: searchTerm, $options: 'i' } },
+      //     { startLocation: { $regex: searchTerm, $option: 'i' } },
+      //     { locations: { $regex: searchTerm, $option: 'i' } },
+      //   ],
+      // })
+
+      */
+
+  /* const searchableFields = ['name', 'startLocation', 'locations']
+  const result = await Tour.find({
+    $or: searchableFields.map((feild) => ({
+      [feild]: { $regex: searchTerm, $options: 'i' },
+    })),
+  }) 17:9 csms3 day-3 p-2
+*/
+
+  const searchableFields = ['name', 'startLocation', 'locations']
+  /*
+  const searchableFields = ['name', 'startLocation', 'locations']
+  const searchQuery = Tour.find({
+    $or: searchableFields.map((feild) => ({
+      [feild]: { $regex: searchTerm, $options: 'i' },
+    })),
+  })
+
+  const result = await searchQuery.find(queryObj)*/
+
+  const searchQuery = Tour.find({
+    $or: searchableFields.map((feild) => ({
+      [feild]: { $regex: searchTerm, $options: 'i' },
+    })),
+  })
+
+  const filterQuery = searchQuery.find(queryObj)
+
+  const page = Number(query?.page) || 1
+  const limit = Number(query?.limit) || 10
+  const skip = (page - 1) * limit
+
+  // const result = await filterQuery.skip(skip).limit(limit)
+  const paginationQuery = filterQuery.skip(skip).limit(limit)
+
+  let sortStr
+
+  if (query?.sortBy && query?.sortOrder) {
+    const sortBy = query?.sortBy
+    const sortOrder = query?.sortOrder
+    sortStr = `${sortOrder === 'desc' ? '-' : ''}${sortBy}`
+  }
+
+  // const result = await paginationQuery.sort(sortStr)
+  const sortQuery = paginationQuery.sort(sortStr)
+
+  let fields = '-v'
+  if (query?.fields) {
+    fields = (query?.fields as string)?.split(',').join(' ')
+  }
+
+  const result = await sortQuery.select(fields)
   return result
 }
 
@@ -32,6 +108,7 @@ const deleteTour = async (id: string) => {
   return result
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getNextSchedule = async (id: string) => {
   const tour = await Tour.getNextNearestStartDateAndEndData()
   //   const nextSchedule = tour?.getNextNearestStartDateAndEndData()
